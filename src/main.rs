030d9e7f4058a9d25d898cb60a78ccf8f68b0f16
Johannes J. Schmidt <jo@die-tf.de>
2016-10-06 09:24:12 +0200

    feat: query global changes

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,19 @@ use std::io::Read;
 use rustc_serialize::json;
 
 #[derive(RustcDecodable, RustcEncodable)]
-pub struct Result {
+pub struct ChangesResult {
+    seq: String,
+    id: String
+}
+
+#[derive(RustcDecodable, RustcEncodable)]
+pub struct Changes {
+    last_seq: String,
+    results: Vec<ChangesResult>,
+}
+
+#[derive(RustcDecodable, RustcEncodable)]
+pub struct DbUpdateResult {
     seq: String,
     db_name: String
 }
@@ -17,7 +29,7 @@ pub struct Result {
 #[derive(RustcDecodable, RustcEncodable)]
 pub struct DbUpdates {
     last_seq: String,
-    results: Vec<Result>,
+    results: Vec<DbUpdateResult>,
 }
 
 fn main() {
@@ -53,7 +65,6 @@ fn main() {
     
     let mut headers = Headers::new();
 
-
     // Gets a value for username if supplied by user, or defaults to "default.conf"
     if let Some(username) = matches.value_of("username") {
         let password = matches.value_of("password").unwrap();
@@ -66,7 +77,7 @@ fn main() {
            }
        ));
     }
-    let url = server_url.to_string() + "/_db_updates";
+    let url = format!("{}/_db_updates", server_url);
     let mut response = client.get(&url).headers(headers).send().unwrap();
 
     assert_eq!(response.status, hyper::Ok);
@@ -78,6 +89,33 @@ fn main() {
     let db_updates: DbUpdates = json::decode(&body).unwrap();
     // println!("decoded {:?}", decoded);
     for result in db_updates.results {
-       println!("{}", result.db_name);
+       if !result.db_name.starts_with("_") {
+         // println!("{}", result.db_name);
+
+         let mut h = Headers::new();
+         // Gets a value for username if supplied by user, or defaults to "default.conf"
+         if let Some(username) = matches.value_of("username") {
+             let password = matches.value_of("password").unwrap();
+             h.set(Authorization(
+                Basic {
+                    username: username.to_owned(),
+                    password: Some(password.to_owned())
+                }
+            ));
+         }
+
+         let u = format!("{}/{}/_changes", server_url, result.db_name);
+         let mut resp = client.get(&u).headers(h).send().unwrap();
+
+         assert_eq!(resp.status, hyper::Ok);
+
+         let mut b = String::new();
+         let res = resp.read_to_string(&mut b);
+         // println!("{}", b);
+         let changes: Changes = json::decode(&b).unwrap();
+         for r in changes.results {
+           println!("{}/{}", result.db_name, r.id);
+         }
+       }
     }
 }
